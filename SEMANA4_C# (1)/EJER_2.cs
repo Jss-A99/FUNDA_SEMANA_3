b8dc5b8ea9afdd69947cf8066ad560cda08a3747
Jss-A99 <noc.aegis@gmail.com>
2025-09-19 09:26:44 -0500

    Avance de s4 en s5

--- SEMANA4_C# (1)/EJER_2.cs
+++ SEMANA4_C# (1)/EJER_2.cs
@@ -10,7 +10,16 @@ namespace SEMANA4_C___1_
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese año: ");
+            int an = int.Parse(Console.ReadLine());
 
+            if ((an % 4 == 0 & an % 100 != 0) || an % 400 == 0)
+                Console.WriteLine("El año es bisiesto.");
+            else 
+                Console.WriteLine("El año no es bisiesto.");
+
+            if (an % 2 == 0) Console.WriteLine("El año es par.");
+            else Console.WriteLine("El año es impar."); 
         }
     }
 }
